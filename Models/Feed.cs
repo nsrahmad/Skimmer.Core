c82f66efbc49f6ac88feb8b38925a38830bfa8e7
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-11-12 18:07:21 +0530

    Initial support for Feed Directories

--- Models/Feed.cs
+++ Models/Feed.cs
@@ -5,14 +5,42 @@ using Nanorm;
 namespace Skimmer.Core.Models;
 
 public class Feed : IDataRecordMapper<Feed>
-{
+{  
+    public static Feed GetFeed(string title, string description, Uri link, int parentId, ICollection<FeedItem>? items, string imageUrl)
+    {
+        return new Feed()
+        {
+            Title = title,
+            Description = description,
+            Link = link,
+            ParentId = parentId,
+            Items = items,
+            ImageUrl = imageUrl,
+        };
+    }
+
+    public static Feed GetFeedDirectory(string title, int parentId, string imageUrl, ICollection<Feed> children)
+    {
+        return new Feed()
+        {
+            Title = title,
+            Description = String.Empty,
+            Link = new Uri("/"),
+            ParentId = parentId,
+            Items = null,
+            Children = children,
+            ImageUrl = imageUrl,
+        };
+    }
+
     public int FeedId { get; set; }
     public required string Title { get; init; }
     public required string Description { get; init; }
     public required Uri Link { get; init; }
-    public int? ParentId { get; set; }
+    public int ParentId { get; set; } = 1;
 
     public ICollection<FeedItem>? Items { get; set; }
+    public ICollection<Feed>? Children { get; set; }
     public required string ImageUrl { get; set; }
 
     public static Feed Map(IDataRecord dataRecord)
@@ -22,9 +50,9 @@ public class Feed : IDataRecordMapper<Feed>
             FeedId = dataRecord.GetInt32(nameof(FeedId)),
             Title = dataRecord.GetString(nameof(Title)),
             Description = dataRecord.GetString(nameof(Description)),
-            Link = new Uri(dataRecord.GetString(nameof(Link))),
+            Link = dataRecord.GetString(nameof(Link)).Equals(String.Empty) ? new Uri("/", UriKind.Relative) : new Uri(dataRecord.GetString(nameof(Link))),
             ImageUrl = dataRecord.GetString(nameof(ImageUrl)),
-            ParentId = dataRecord.IsDBNull(nameof(ParentId)) ? null : dataRecord.GetInt32(nameof(ParentId))
+            ParentId = dataRecord.GetInt32(nameof(ParentId))
         };
     }
 }
